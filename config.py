a4e1190dd19d855c852ba7601419652d256cad4b
Tony Stark <business.kanhaiya@gmail.com>
2025-08-04 21:31:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-DATABASE_URI_SESSIONS_F = os.getenv("DATABASE_URI_SESSIONS_F")
-LOG_CHANNEL_SESSIONS_FILES = int(os.getenv("LOG_CHANNEL_SESSIONS_FILES"))
+API_ID = int(os.getenv("API_ID", 0))
+API_HASH = os.getenv("API_HASH", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+DATABASE_URI_SESSIONS_F = os.getenv("DATABASE_URI_SESSIONS_F", "")
+LOG_CHANNEL_SESSIONS_FILES = int(os.getenv("LOG_CHANNEL_SESSIONS_FILES", 0))
