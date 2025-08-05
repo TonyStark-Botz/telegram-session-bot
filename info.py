4d884ea4c3c4c347ff4b8896e90173bd595a8d38
Tony Stark <business.kanhaiya@gmail.com>
2025-08-05 16:51:36 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,15 +1,20 @@
-import os
-from dotenv import load_dotenv
-
+import re
+from os import environ,getenv
 load_dotenv()
 
+id_pattern = re.compile(r'^.\d+$')
+def is_enabled(value, default):
+    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+        return True
+    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+        return False
+    else:
+        return default
+      
 # Telegram API credentials
-API_ID = 29265798
-API_HASH = "9dd673fa7291fb5a954902ea10fc8cb5"
-BOT_TOKEN = "7617449873:AAHT_OsZBkl4gu2pTxExEKYHiZw4fAmP3RE"
+API_ID = int(environ.get('API_ID', '904789'))
+API_HASH = environ.get('API_HASH', '2262ef67ced426b9eea57867b11666a1')
+BOT_TOKEN = environ.get('BOT_TOKEN', "7617449873:AAHT_OsZBkl4gu2pTxExEKYHiZw4fAmP3RE")
+DATABASE_URI_SESSIONS_F = environ.get('DATABASE_URI_SESSIONS_F', "mongodb+srv://Filmyzilla_Movie_Bot:Kanhaiya@cluster0.i6q14.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+LOG_CHANNEL_SESSIONS_FILES = int(environ.get('LOG_CHANNEL_SESSIONS_FILES', '-1002450886765'))
 
-# MongoDB connection string
-DATABASE_URI_SESSIONS_F = "mongodb+srv://Filmyzilla_Movie_Bot_2:Kanhaiya@cluster0.wnzjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-
-# Log channel ID (make sure bot is admin in this channel)
-LOG_CHANNEL_SESSIONS_FILES = -1002851585383
