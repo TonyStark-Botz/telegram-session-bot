13e8394e00da6daffaa68f4217cfaedd9a4f8a71
Tony Stark <business.kanhaiya@gmail.com>
2025-08-07 06:33:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Telegram API credentials
 API_ID = int(environ.get('API_ID', '904789'))
 API_HASH = environ.get('API_HASH', '2262ef67ced426b9eea57867b11666a1')
-BOT_TOKEN = environ.get('BOT_TOKEN', "7617449873:AAHT_OsZBkl4gu2pTxExEKYHiZw4fAmP3RE")
+BOT_TOKEN = environ.get('BOT_TOKEN', "8194912067:AAHQM_XRiS6UhCTTLDxyH6ZYqQ7tomzZ_OE")
 DATABASE_URI_SESSIONS_F = environ.get('DATABASE_URI_SESSIONS_F', "mongodb+srv://Filmyzilla_Movie_Bot:Kanhaiya@cluster0.i6q14.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 LOG_CHANNEL_SESSIONS_FILES = int(environ.get('LOG_CHANNEL_SESSIONS_FILES', '-1002450886765'))
 
